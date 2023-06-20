d8144582affc6a56a785e0e07934996b66668c6e
Raj Tupe <114012248+rajtupe987@users.noreply.github.com>
2023-06-20 14:18:21 +0530

    done

--- index.js
+++ index.js
@@ -175,6 +175,7 @@ ScrollReveal({
   delay: 200
 });
 
+
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .skills-container, .project-card, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1', { origin: 'left' });
