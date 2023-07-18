939cf0012e2c54f1f768652032cae5ca7108a77f
Raj Tupe <114012248+rajtupe987@users.noreply.github.com>
2023-07-18 15:15:15 +0530

    solved with issue

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 
 
-// let menuIcon = document.querySelector('#menu-icon');
-// let navbar = document.querySelector('#nav-menu');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('#nav-menu');
 
-// menuIcon.onclick = () => {
-//   menuIcon.classList.toggle('bx-x');
-//   navbar.classList.toggle('active');
-// };
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
 
 
 //*=============================== toggle icon navbar ==================================
 
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('#nav-menu')
+// let menuIcon = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('#nav-menu')
 
-menuIcon.onclick = () => {
-  menuIcon.classList.toggle('bx-x');
-  navbar.classList.toggle('active')
-};
+// menuIcon.onclick = () => {
+//   menuIcon.classList.toggle('bx-x');
+//   navbar.classList.toggle('active')
+// };
 
 // *=============================== github calendar ==================================
 GitHubCalendar(".calendar", "RajTupe");
@@ -25,34 +25,16 @@ GitHubCalendar(".calendar", "RajTupe");
 // or enable responsive functionality:
 GitHubCalendar(".calendar", "RajTupe", { responsive: true });
 
-//*=============================== github calender ==================================
-GitHubCalendar(".calendar", "RajTupe");
+// //*=============================== github calender ==================================
+// GitHubCalendar(".calendar", "RajTupe");
 
-//or enable responsive functionality:
-GitHubCalendar(".calendar", "RajTupe", { responsive: true });
-
-
-// // *=============================== resume open in new tab ==================================
-// let resume1 = document.getElementById("resume-button-1").addEventListener("click", NewTab);
-// let resume2 = document.getElementById("resume-button-2").addEventListener("click", NewTab);
-
-// function NewTab() {
-//   window.open(
-//     "https://drive.google.com/file/d/1aIx7oXswHginslfjiUfM6fRxb4gcTBjX/view?usp=sharing",
-//     "_blank"
-//   );
-// }
-
-// // *=============================== scroll sections active link ==================================
-// let sections = document.querySelectorAll('section');
-// let navlinks = document.querySelectorAll('header nav a');
+// //or enable responsive functionality:
+// GitHubCalendar(".calendar", "RajTupe", { responsive: true });
 
 
 // *=============================== resume open in new tab ==================================
-
-
-let resume1 = document.getElementById("resume-button-1").addEventListener("click", NewTab)
-let resume2 = document.getElementById("resume-button-2").addEventListener("click", NewTab)
+let resume1 = document.getElementById("resume-button-1").addEventListener("click", NewTab);
+let resume2 = document.getElementById("resume-button-2").addEventListener("click", NewTab);
 
 function NewTab() {
   window.open(
@@ -63,31 +45,25 @@ function NewTab() {
 
 // *=============================== scroll sections active link ==================================
 let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nav a')
+let navlinks = document.querySelectorAll('header nav a');
 
-// window.onscroll = () => {
-//   sections.forEach(sec => {
-//     let top = window.scrollY;
-//     let offset = sec.offsetTop - 150;
-//     let height = sec.offsetHeight;
-//     let id = sec.getAttribute('id');
 
-//     if (top >= offset && top < offset + height) {
-//       navlinks.forEach(links => {
-//         links.classList.remove('active');
-//         document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
-//       });
-//     }
-//   });
+// *=============================== resume open in new tab ==================================
 
-//   // *=============================== sticky navbar ==================================
-//   let header = document.querySelector('header');
-//   header.classList.toggle('sticky', window.scrollY > 100);
 
-//   // *=============================== remove toggle icon and navbar when click navbar link(scroll) ==================================
-//   menuIcon.classList.remove('bx-x');
-//   navbar.classList.remove('active');
-// };
+// let resume1 = document.getElementById("resume-button-1").addEventListener("click", NewTab)
+// let resume2 = document.getElementById("resume-button-2").addEventListener("click", NewTab)
+
+// function NewTab() {
+//   window.open(
+//     "https://drive.google.com/file/d/1aIx7oXswHginslfjiUfM6fRxb4gcTBjX/view?usp=sharing",
+//     "_blank"
+//   );
+// }
+
+// // *=============================== scroll sections active link ==================================
+// let sections = document.querySelectorAll('section');
+// let navlinks = document.querySelectorAll('header nav a')
 
 window.onscroll = () => {
   sections.forEach(sec => {
@@ -97,25 +73,49 @@ window.onscroll = () => {
     let id = sec.getAttribute('id');
 
     if (top >= offset && top < offset + height) {
-        navlinks.forEach(links => {
-          links.classList.remove('active');
-          document.querySelector('header nav a[href*="" + id + ""]').classList.add('active')
-        });
-    };
-
+      navlinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
+      });
+    }
   });
-  // *=============================== sticky navbar ==================================
-   
-  let header = document.querySelector('header');
 
+  // *=============================== sticky navbar ==================================
+  let header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 100);
 
-// *=============================== remove toggle icon and navbar when click navbar link(scroll) ==================================
-
-menuIcon.classList.remove('bx-x');
-navbar.classList.remove('active')
-
+  // *=============================== remove toggle icon and navbar when click navbar link(scroll) ==================================
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
 };
+
+// window.onscroll = () => {
+//   sections.forEach(sec => {
+//     let top = window.scrollY;
+//     let offset = sec.offsetTop - 150;
+//     let height = sec.offsetHeight;
+//     let id = sec.getAttribute('id');
+
+//     if (top >= offset && top < offset + height) {
+//         navlinks.forEach(links => {
+//           links.classList.remove('active');
+//           document.querySelector('header nav a[href*="" + id + ""]').classList.add('active')
+//         });
+//     };
+
+//   });
+//   // *=============================== sticky navbar ==================================
+   
+//   let header = document.querySelector('header');
+
+//   header.classList.toggle('sticky', window.scrollY > 100);
+
+// // *=============================== remove toggle icon and navbar when click navbar link(scroll) ==================================
+
+// menuIcon.classList.remove('bx-x');
+// navbar.classList.remove('active')
+
+// };
 
 // *=============================== scroll reveal ==================================
 
